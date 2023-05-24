2cc180613b4ead436741403e16ecd8485d8060b2
Martín Morondo <martinmorondo@gmail.com>
2023-05-24 16:09:29 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react';
+import './index.css';
+import 'animate.css';
+
+function App() {
+
+  return (
+      <div className='App'>
+        <h1 className='animate__animated animate__fadeIn'>Preguntados</h1>
+      </div>
+  )
+}
+
+export default App
