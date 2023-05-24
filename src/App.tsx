ace27a526b96e2fe63e8ed8fa1472a3024fc64db
Martín Morondo <martinmorondo@gmail.com>
2023-05-24 16:27:07 -0300

    add project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,209 @@
-// import { useState } from 'react';
-import './index.css';
+import { useState, useEffect } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import questions from './data/questions';
 import 'animate.css';
+import correctAnswer from './sounds/correctAnswer.mp3';
 
 function App() {
+  const [currentCategory, setCurrentCategory] = useState<string | null>(null);
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
+  const [feedback, setFeedback] = useState<string | null>(null);
+  const [timeRemaining, setTimeRemaining] = useState<number>(10);
+  const [showHint, setShowHint] = useState<boolean>(false);
+  const [progress, setProgress] = useState<number>(100);
+  const [gameOver, setGameOver] = useState<boolean>(false);
 
+  const categoryQuestions = questions[currentCategory];
+  const question = categoryQuestions && categoryQuestions[currentQuestion];
+
+  const handleHint = () => {
+    setShowHint(true);
+  };
+
+  const handleAnswer = (selectedAnswer: string) => {
+    if (gameOver) {
+      return; // Si el juego ha terminado, no realizar ninguna acción
+    }
+
+    if (question && selectedAnswer === question.answer) {
+      setScore((prevScore) => prevScore + 1);
+      setFeedback('¡Respuesta correcta!');
+      playCorrectSound();
+      toast.success('¡Respuesta correcta!', { autoClose: 2000 });
+    } else {
+      setFeedback('Respuesta incorrecta');
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (categoryQuestions && nextQuestion < categoryQuestions.length) {
+      setCurrentQuestion(nextQuestion);
+      setTimeRemaining(10);
+    } else {
+      setGameOver(true);
+    }
+  };
+
+  const playCorrectSound = () => {
+    const audio = new Audio(correctAnswer);
+    audio.play();
+  };
+
+  useEffect(() => {
+    if (feedback === '¡Respuesta correcta!') {
+      playCorrectSound();
+    }
+  }, [feedback]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeRemaining((prevTime) => {
+        if (prevTime === 0) {
+          handleAnswer(''); // Si el tiempo se agota, se considera una respuesta incorrecta
+          return 0;
+        } else {
+          const newProgress = (prevTime / 10) * 100; // Calcula el progreso en base al tiempo restante
+          setProgress(newProgress); // Actualiza el progreso de la barra
+          return prevTime - 1;
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [currentQuestion]);
+
+  const handleCategoryChange = (category: string) => {
+    setCurrentCategory(category);
+    setCurrentQuestion(0);
+    setScore(0);
+    setFeedback(null);
+    setTimeRemaining(10);
+    setShowHint(false);
+    setGameOver(false);
+  };
+
+  const handleRestartGame = () => {
+    setCurrentCategory(null);
+    setCurrentQuestion(0);
+    setScore(0);
+    setFeedback(null);
+    setTimeRemaining(10);
+    setShowHint(false);
+    setGameOver(false);
+  };
+
+  useEffect(() => {
+    const animateElements = document.querySelectorAll('.animate__animated');
+
+    const showElements = () => {
+      animateElements.forEach((element) => {
+        element.classList.add('visible');
+      });
+    };
+
+    const hideElements = () => {
+      animateElements.forEach((element) => {
+        element.classList.remove('visible');
+      });
+    };
+
+   
+    // Mostrar elementos animados al cargar la página
+    showElements();
+
+    // Ocultar elementos animados después de 2 segundos
+    setTimeout(hideElements, 2000);
+
+    // Limpiar clases de animación al finalizar
+    setTimeout(() => {
+      animateElements.forEach((element) => {
+        element.classList.remove('animate__animated', 'animate__fadeIn');
+      });
+    }, 3000);
+  }, []);
+  
   return (
-      <div className='App'>
-        <h1 className='animate__animated animate__fadeIn'>Preguntados</h1>
-      </div>
-  )
+    <div className="App">
+      <h1 className="animate__animated animate__fadeIn">Preguntados</h1>
+      <ToastContainer />
+      {currentCategory ? (
+        <>
+          {categoryQuestions && categoryQuestions.length > 0 && !gameOver ? (
+            <div className="animate__animated animate__fadeIn">
+              <h2>{question.question}</h2>
+              {showHint && (
+                <p className="hint animate__animated animate__fadeIn">Pista: {question.hint}</p>
+              )}
+              <ul>
+                {question.options.map((option, optionIndex) => (
+                  <li
+                    key={optionIndex}
+                    onClick={() => handleAnswer(option)}
+                    className="animate__animated animate__fadeIn"
+                  >
+                    {option}
+                  </li>
+                ))}
+              </ul>
+              {!showHint && (
+                <button onClick={handleHint} className="hint-question animate__animated animate__fadeIn">
+                  Mostrar Pista
+                </button>
+              )}
+              {feedback && <p className="animate__animated animate__fadeIn">{feedback}</p>}
+              <p className="animate__animated animate__fadeIn">
+                Tiempo restante: {timeRemaining} segundos
+              </p>
+              <div
+                className="progress-bar animate__animated animate__fadeIn"
+                style={{ width: `${progress}%` }}
+              ></div>
+              <p className="animate__animated animate__fadeIn">Puntuación: {score}</p>
+            </div>
+          ) : (
+            <div className="resultados animate__animated animate__fadeIn">
+              <h2>Resultados</h2>
+              <p>Puntuación final: {score}</p>
+              <button className="btn-restart" onClick={handleRestartGame}>
+                Reiniciar juego
+              </button>
+            </div>
+          )}
+        </>
+      ) : (
+        <div>
+              <h2 className="animate__animated animate__fadeIn">Elige una categoría</h2>
+          <ul>
+            <li
+              onClick={() => handleCategoryChange('Geografía')}
+              className="animate__animated animate__fadeIn"
+            >
+              Geografía
+            </li>
+            <li
+              onClick={() => handleCategoryChange('Deportes')}
+              className="animate__animated animate__fadeIn"
+            >
+              Deportes
+            </li>
+            <li
+              onClick={() => handleCategoryChange('Historia')}
+              className="animate__animated animate__fadeIn"
+            >
+              Historia
+            </li>
+            <li
+              onClick={() => handleCategoryChange('Programación')}
+              className="animate__animated animate__fadeIn"
+            >
+              Programación
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/data/questions.js
@@ -0,0 +1,116 @@
+const questions = {
+    Geografía: [
+      {
+        question: '¿Cuál es la capital de Francia?',
+        options: ['París', 'Roma', 'Barcelona'],
+        answer: 'París',
+        hint: 'Es conocida como "La Ciudad del Amor"',
+      },
+      {
+        question: '¿Cuál es la capital de Italia?',
+        options: ['París', 'Roma', 'Barcelona'],
+        answer: 'Roma',
+        hint: 'En esta ciudad se encuentra el Coliseo',
+      },
+      {
+        question: '¿Cuál es la capital de España?',
+        options: ['Madrid', 'Londres', 'Berlín'],
+        answer: 'Madrid',
+        hint: 'En esta ciudad se encuentra el Palacio Real',
+      },
+      {
+        question: '¿Cuál es el río más largo del mundo?',
+        options: ['Amazonas', 'Nilo', 'Yangtsé'],
+        answer: 'Amazonas',
+        hint: 'Fluye a través de América del Sur',
+      },
+    ],
+    Deportes: [
+      {
+        question: '¿En qué deporte se utiliza una pelota de baloncesto?',
+        options: ['Baloncesto', 'Fútbol', 'Tenis'],
+        answer: 'Baloncesto',
+        hint: 'El aro se encuentra a una altura de 3.05 metros',
+      },
+      {
+        question: '¿En qué deporte se utiliza un palo y una pelota?',
+        options: ['Golf', 'Hockey', 'Béisbol'],
+        answer: 'Golf',
+        hint: 'El objetivo es embocar la pelota en un hoyo en el menor número de golpes posibles',
+      },
+      {
+        question: '¿Cuál es el deporte más popular en Estados Unidos?',
+        options: ['Fútbol americano', 'Béisbol', 'Baloncesto'],
+        answer: 'Fútbol americano',
+        hint: 'El Super Bowl es el evento más importante de este deporte',
+      },
+      {
+        question: '¿Cuál es el ganador más exitoso en la historia de los Juegos Olímpicos?',
+        options: ['Estados Unidos', 'China', 'Unión Soviética'],
+        answer: 'Estados Unidos',
+        hint: 'Ha ganado la mayor cantidad de medallas en total',
+      },
+    ],
+    Historia: [
+      {
+        question: '¿En qué año ocurrió la Revolución Francesa?',
+        options: ['1776', '1789', '1812'],
+        answer: '1789',
+        hint: 'Este evento marcó el fin de la monarquía absoluta en Francia',
+      },
+      {
+        question: '¿Cuál fue la primera civilización mesopotámica?',
+        options: ['Egipto', 'Grecia', 'Sumeria'],
+        answer: 'Sumeria',
+        hint: 'Se desarrolló en la región de la antigua Mesopotamia',
+      },
+      {
+        question: '¿En qué año se descubrió América?',
+        options: ['1492', '1500', '1520'],
+        answer: '1492',
+        hint: 'El navegante Cristóbal Colón realizó su primer viaje transatlántico en este año',
+      },
+      {
+        question: '¿Cuál fue la primera dinastía china?',
+        options: ['Qin', 'Han', 'Shang'],
+        answer: 'Qin',
+        hint: 'Esta dinastía unificó China por primera vez',
+  },
+  {
+        question: '¿Cuál fue el líder de la Revolución Rusa?',
+        options: ['Vladimir Lenin', 'Joseph Stalin', 'Mikhail Gorbachev'],
+        answer: 'Vladimir Lenin',
+        hint: 'Fue el fundador del partido bolchevique y el primer presidente del Gobierno Soviético',
+  },
+  ],
+  Programación: [
+  {
+        question: '¿Qué lenguaje de programación se utiliza para desarrollar aplicaciones web?',
+        options: ['Java', 'Python', 'JavaScript'],
+        answer: 'JavaScript',
+        hint: 'Lenguaje de programación que debes usar para añadir características interactivas a tu sitio web',
+  },
+  {
+        question: '¿Qué significa HTML?',
+        options: ['HyperText Markup Language', 'High Tech Machine Learning', 'Home Tooling Mechanism Language'],
+        answer: 'HyperText Markup Language',
+        hint: 'Es un lenguaje de marcado utilizado para la estructura y presentación de contenidos en la web',
+  },
+  {
+        question: '¿Cuál es el framework más popular para desarrollo de aplicaciones móviles?',
+        options: ['React Native', 'Angular', 'Flutter'],
+        answer: 'React Native',
+        hint: 'Permite crear aplicaciones móviles multiplataforma utilizando JavaScript y React',
+  },
+  {
+        question: '¿Cuál es el lenguaje de programación más utilizado para el desarrollo de inteligencia artificial?',
+        options: ['Python', 'Java', 'C++'],
+        answer: 'Python',
+        hint: 'Es conocido por su simplicidad y amplio conjunto de bibliotecas para el aprendizaje automático',
+  },
+  ],
+  };
+  
+  export default questions;
+  
+  
