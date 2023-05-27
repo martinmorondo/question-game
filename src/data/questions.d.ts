ed6b9f7a24ef7807b0d4f0334e87490d15c496cd
Martín Morondo <martinmorondo@gmail.com>
2023-05-26 21:59:05 -0300

    add file

---
+++ src/data/questions.d.ts
@@ -0,0 +1,11 @@
+declare const questions: {
+    [key: string]: {
+      question: string;
+      options: string[];
+      answer: string;
+      hint: string;
+    }[];
+  };
+  
+  export default questions;
+  
